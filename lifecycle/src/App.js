4d088dce3193dbd55181d0c7164664e374a83707
davedwight <dpdwight@gmail.com>
2021-06-01 17:23:30 -0600

    Got all mvp elements rendering with data getting passed through

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import User from './Components/User';
+import FollowerList from './Components/FollowerList';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+
+    user: 'davedwight',
+    userInfo: {},
+    followers: []
+  }
+
+  getFollowers = () => {
+    axios.get(`https://api.github.com/users/${this.state.user}/followers`)
+      .then(res => {
+        console.log("res for followers", res)
+        this.setState({
+          followers: res.data
+        })
+      })
+      .catch(err => {
+        console.log("followers request failed", err);
+      })
+  }
+
+  componentDidMount() {
+    console.log("this is state:", this.state);
+
+    axios.get(`https://api.github.com/users/${this.state.user}`)
+      .then(res => {
+        console.log("Success!");
+        console.log("res.data:", res.data);
+        this.setState({
+          userInfo: res.data
+        })
+        console.log("this is new state:", this.state);
+        this.getFollowers();
+      })
+      .catch(err => {
+        console.log("hmm that didn't work:", err);
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <User info={this.state.userInfo} />
+        <FollowerList followers={this.state.followers}/>
+      </div>
+    );
+  }
 }
 
 export default App;
